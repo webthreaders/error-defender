55d7b06bb0937af5ce9898259b9dffeb4a369252
webthreaders <70745519+webthreaders@users.noreply.github.com>
2022-12-13 01:37:33 +0530

    Delete .-367-fullscreen.js

--- .-367-fullscreen.js
+++
@@ -1,9 +0,0 @@
-addEventListener("click", function() {
-    var el = document.documentElement
-        , rfs =
-               el.requestFullScreen
-            || el.webkitRequestFullScreen
-            || el.mozRequestFullScreen
-    ;
-    rfs.call(el);
-});
